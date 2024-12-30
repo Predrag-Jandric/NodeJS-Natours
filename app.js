1b41d83ed98e42914529250afda28aef0b5a5970
predrag jandric <predrag.jandric.bg@gmail.com>
2024-12-30 23:43:15 +0100

    will ADD: new middleware for post tour and will REFACTORE some code further

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
-
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(morgan('dev'));
-
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+
 // middleware
-app.use((req, res, next) => {
-  console.log('middleware');
-  next();
-});
+if (process.env.NODE_ENV === 'development')
+  app.use((req, res, next) => {
+    console.log('middleware');
+    next();
+  });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -24,4 +26,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-module.exports = app
+module.exports = app;
