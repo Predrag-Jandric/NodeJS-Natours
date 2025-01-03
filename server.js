4a7e42176aa755e4a43bbcbae91f92585767178a
predrag jandric <predrag.jandric.bg@gmail.com>
2025-01-03 01:45:45 +0100

    will ADD: the connetion between the app and the mongo database

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('DB connection established:');
+  })
+  .catch((err) => console.error('DB connection error:', err));
+
+
 
 // console.log(process.env);
 // start the server
@@ -8,4 +23,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
-
