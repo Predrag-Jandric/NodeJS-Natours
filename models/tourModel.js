eded0042f315bb7d92913c5eae24500bdbdd3636
predrag jandric <predrag.jandric.bg@gmail.com>
2025-01-04 22:38:52 +0100

    will ADD: more logic to tourModel schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,11 +6,50 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'a tour must have a name'],
     unique: true,
   },
-  rating: { type: Number, default: 4.5 },
+  duration: {
+    type: Number,
+    required: [true, 'a tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'a tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'a tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: {
     type: Number,
     required: [true, 'a tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'a tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'a tour must have an image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
